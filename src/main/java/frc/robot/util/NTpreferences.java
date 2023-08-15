aefa68df9f849a29f52cb91b8c5d15bd5c51667d
Maddox <rynvnskl@yahoo.com>
2023-08-15 18:59:49 -0400

    Fixed bug with intake of rotation offsets.

--- src/main/java/frc/robot/util/NTpreferences.java
+++ src/main/java/frc/robot/util/NTpreferences.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
 
@@ -21,10 +22,15 @@ public class NTpreferences {
     public static void loadPreferences() {
         loadedCorrectly = true;
         
-        universalOffsets.put("3U", Preferences.getDouble("Module 3 Offset Universal", 0));
-        universalOffsets.put("0U", Preferences.getDouble("Module 0 Offset Universal", 0));
-        universalOffsets.put("2U", Preferences.getDouble("Module 2 Offset Universal", 0));
-        universalOffsets.put("1U", Preferences.getDouble("Module 1 Offset Universal", 0));
+        universalOffsets.put("3U", Rotation2d.fromRotations(Preferences.getDouble("Module 3 Offset Universal", 0)).getDegrees());
+        universalOffsets.put("0U", Rotation2d.fromRotations(Preferences.getDouble("Module 0 Offset Universal", 0)).getDegrees());
+        universalOffsets.put("2U", Rotation2d.fromRotations(Preferences.getDouble("Module 2 Offset Universal", 0)).getDegrees());
+        universalOffsets.put("1U", Rotation2d.fromRotations(Preferences.getDouble("Module 1 Offset Universal", 0)).getDegrees());
+
+        SmartDashboard.putNumber("Mod 3", Rotation2d.fromDegrees(universalOffsets.get("3U")).getRotations());
+        SmartDashboard.putNumber("Mod 0", Rotation2d.fromDegrees(universalOffsets.get("0U")).getRotations());
+        SmartDashboard.putNumber("Mod 2", Rotation2d.fromDegrees(universalOffsets.get("2U")).getRotations());
+        SmartDashboard.putNumber("Mod 1", Rotation2d.fromDegrees(universalOffsets.get("1U")).getRotations());
 
         for (Double angle : universalOffsets.values()) {
             if ((angle <= 0 || angle >= 360) && Robot.isReal()) loadedCorrectly = false;

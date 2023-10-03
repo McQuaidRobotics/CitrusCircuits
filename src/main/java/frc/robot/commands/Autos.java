75e5be0926d1c2f5bc2690851eb00bed5e9000da
Maddox <rynvnskl@yahoo.com>
2023-10-02 23:27:28 -0400

    Added map of marker events for path planner. Going to bed now :3

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,9 +1,8 @@
 package frc.robot.commands;
 
-import javax.lang.model.element.ElementVisitor;
+import java.util.HashMap;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.super_structure.elevator.Elevator;
 import frc.robot.subsystems.super_structure.pivot.Pivot;
@@ -17,21 +16,38 @@ public class Autos {
         TWO_GAME_PIECE_CHARGE_STATION
     }
 
+    private static final HashMap<String, Command> EVENT_MARKERS = new HashMap<String, Command>() {{{
+        //TODO {Maddox} Unsure where robot's cube/cone mode is in code base, but
+        //need a way to switch between the modes in auto
+        put("placeHighAuto", new InstantCommand());
+        put("placeMidAuto", new InstantCommand());
+        put("placeLowAuto", new InstantCommand());
+
+        put("pickUpGroundAuto", new InstantCommand());
+        put("pickUpStationAuto", new InstantCommand());
+
+        put("activateRollers", new InstantCommand());
+        put("deactivateRollers", new InstantCommand());
+
+        put("useConeMode", new InstantCommand());
+        put("useCubeMode", new InstantCommand());
+    }}};
+
     public static Command getAutoPathCommand(AutoPaths autoPath, Swerve swerve, Elevator elevator, Pivot pivot, Wrist wrist) {
         Command autoCommand = new InstantCommand();
         switch (autoPath) {
             case THREE_GAME_PIECE: autoCommand = commandThreeGamePiece(swerve, elevator, pivot, wrist);
             case TWO_GAME_PIECE_CHARGE_STATION: autoCommand = commandTwoGamePieceChargeStation(swerve, elevator, pivot, wrist);
-            case NOTHING: autoCommand = new InstantCommand();
+            case NOTHING:
         };
         return autoCommand;
     }
 
-    public static Command commandThreeGamePiece(Swerve swerve, Elevator elevator, Pivot pivot, Wrist wrist) {
+    private static Command commandThreeGamePiece(Swerve swerve, Elevator elevator, Pivot pivot, Wrist wrist) {
         return new InstantCommand().withName("commandThreeGamePiece");
     } 
 
-    public static Command commandTwoGamePieceChargeStation(Swerve swerve, Elevator elevator, Pivot pivot, Wrist wrist) {
+    private static Command commandTwoGamePieceChargeStation(Swerve swerve, Elevator elevator, Pivot pivot, Wrist wrist) {
         return new InstantCommand().withName("commandTwoGamePieceChargeStation");
     }
 }

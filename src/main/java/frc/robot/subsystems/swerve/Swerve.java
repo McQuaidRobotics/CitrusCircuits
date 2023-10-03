72d04f5702614c8d40eb2a4a9ee6957d0b93796d
Maddox <rynvnskl@yahoo.com>
2023-10-03 13:15:08 -0400

    Added commandRunPath to Swerve.java

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -2,6 +2,10 @@ package frc.robot.subsystems.swerve;
 
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -13,8 +17,16 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.kAuto;
+import frc.robot.Constants.kSwerve;
 
 public class Swerve extends SubsystemBase {
     private SwerveDriveOdometry swerveOdometry;
@@ -58,6 +70,10 @@ public class Swerve extends SubsystemBase {
         }
     }
 
+    public Command commandStopDrives() {
+        return new InstantCommand(() -> setModuleStates(new ChassisSpeeds()));
+    }
+
     public void zeroGyro() {
         gyro.setYaw(0.0);
     }
@@ -83,6 +99,10 @@ public class Swerve extends SubsystemBase {
         }
     }
 
+    public void setModuleStates(ChassisSpeeds chassisSpeeds) {
+        setModuleStates(kSwerve.SWERVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds));
+    }
+
     public SwerveModuleState[] getModuleStates() {
         SwerveModuleState[] states = new SwerveModuleState[4];
         for (SwerveModule module : mSwerveMods) {
@@ -97,6 +117,33 @@ public class Swerve extends SubsystemBase {
 
     public void resetOdometry(Pose2d pose) {
         swerveOdometry.resetPosition(getYaw(), getModulePositions(), pose);
+    }
+
+    public PathPlannerTrajectory openFilePath(String autoPathFile) {
+        PathPlannerTrajectory autoPath = PathPlanner.loadPath(
+            autoPathFile, 
+            new PathConstraints(kSwerve.MAX_SPEED, kSwerve.MAX_ANGULAR_VELOCITY));
+        return autoPath;
+    }
+
+    public Command commandRunPath(String autoPathFileName, boolean resetOdometry) {
+        PathPlannerTrajectory autoPath = openFilePath(autoPathFileName);
+        return new SequentialCommandGroup(
+            new InstantCommand(() -> {
+                if (resetOdometry) this.resetOdometry(autoPath.getInitialHolonomicPose());
+            }),
+            new PPSwerveControllerCommand(
+                autoPath,
+                this::getPose,
+                kSwerve.SWERVE_KINEMATICS,
+                kAuto.AUTO_X_PID,
+                kAuto.AUTO_Y_PID,
+                kAuto.AUTO_ANGULAR_PID,
+                this::setModuleStates,
+                this
+            ),
+            commandStopDrives()
+        ).withName("commandRunPath: " + autoPathFileName);
     }
 
     @Override
